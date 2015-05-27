306cc80c62e39f164ff3ea132a81a2f403dbdf67
Krzysztof <krzysztof.klimczyk@gmail.com>
2015-05-27 23:05:27 +0100

    dynamic meta tag

--- script.js
+++ script.js
@@ -52,6 +52,8 @@ var main = function() {
 		$('#dialogboxBody').append('<iframe width="450" height="315" src="' + victoryVideoLink + '?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
 		$('#dialogboxFooter').html('<div onclick="" class="buttons playAgain"><p class="noselect">Play again</p></div>');
 		$('#dialogboxFooter').append('<div onclick="" class="buttons share-button"><p class="noselect"><a href="https://www.facebook.com/sharer/sharer.php?u=http://www.one-click-game.com">Share this victory!</a></p></div>');
+
+		$('meta[name=description]').attr('content', victoryVideoLink);
 	    }
 	    else {
 		var lostGameMessage = pickRandomFromArray(this.gameOverMessages);
